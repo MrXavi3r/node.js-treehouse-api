a5d978c513db2c89a53277be4cfdee6cdd9b42ea
Xavier Ritch <xritch89@hotmail.com>
2020-09-26 13:58:57 -0400

    made a few helpful comments

--- app.js
+++ app.js
@@ -2,5 +2,9 @@
 
 const profile = require('./profile')
 
+
+//stores arguments passed to command line in an array for use in command line 
 const users = process.argv.slice(2);
+
+//loops over users array and passes arguments to get function in profile.js
 users.forEach(profile.get);
